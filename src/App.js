773f0ff7ad623ccb59cd5be038296c8c6637de48
Aziz Ch <98626462+M0Aziz@users.noreply.github.com>
2023-02-19 20:55:40 +0100

    Atelier n°2 Partie 1 TERMINER
    Atelier n°0   ==>   Atelier n°2 Partie 1 TERMINER

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavbarComponent from "./Components/NavbarComponent";
+import Products from "./Components/Products";
+
+function App() {
+  return (
+    <div className="App">
+  
+      <NavbarComponent />
+      <br></br>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
